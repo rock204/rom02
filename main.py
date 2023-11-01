edb8bfae998413833390d109862d97c8b263bdcb
rock204 <75961408+rock204@users.noreply.github.com>
2023-11-01 16:12:31 +0900

    測定開始の位置を取得を追加

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-def on_button_pressed_a(): #Aボタンで測定開始
-    global mode
-    mode = 1
+def get_start_position():
+    global bf,m_deg,deg_180,deg_0,start_deg
     OLED12864_I2C.clear()
-    OLED12864_I2C.show_string(0, 0, "Start Measur.", 1)
+    OLED12864_I2C.show_string(0, 1, "move to St.", 1)
+    basic.show_icon(IconNames.ASLEEP)
+    # タクトスイッチが押されるまで待つ
+    while pins.digital_read_pin(DigitalPin.P1) == 0:
+        pass
+    bf = pins.analog_read_pin(AnalogPin.P0) #可変抵抗の電圧を取得
+    #取得したアナログ値をキャリブレーション値で角度に変換
+    m_deg = Math.map(bf, deg_0, deg_180, 0, 180)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+    bf=Math.round(m_deg) #角度を四捨五入
+    string_bf=bf+" deg"
+    OLED12864_I2C.clear()
+    OLED12864_I2C.show_string(0, 0,'Start deg', 1)
+    OLED12864_I2C.show_string(0, 1,string_bf, 1)
+    start_deg=bf
+
+def on_button_pressed_a():
+    global mode
+    # Aボタンで測定開始
+    #mode = 1
+    OLED12864_I2C.clear()
+    OLED12864_I2C.show_string(0, 0, "Start Position.", 1)
+    get_start_position()
+    mode=1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_ab(): #A+Bボタンでキャリブレーション呼び出し
-    basic.show_string("reset ROM")
+def on_button_pressed_ab():
+    # A+Bボタンでキャリブレーション呼び出し
     syokika()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_button_pressed_b(): #Bボタンで測定角度再表示
-    basic.show_number(Math.round(m_deg))
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def syokika(): #キャリブレーション
+def syokika():
+    global deg_0, deg_180, mode
+    # キャリブレーション
     OLED12864_I2C.clear()
     OLED12864_I2C.show_string(0, 0, "Start Cal", 1)
-    global deg_0, deg_180, mode
     OLED12864_I2C.show_string(0, 1, "move to 0", 1)
-    #basic.show_string("move 0")
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
     while pins.digital_read_pin(DigitalPin.P1) == 0:
         pass
     deg_0 = pins.analog_read_pin(AnalogPin.P0)
     OLED12864_I2C.show_string(0, 1, "move to 180", 1)
-    #basic.show_string("move 180")
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
     while pins.digital_read_pin(DigitalPin.P1) == 0:
@@ -41,36 +59,47 @@ def syokika(): #キャリブレーション
     OLED12864_I2C.clear()
     OLED12864_I2C.show_string(0, 0, "Measurement  Ready", 1)
     OLED12864_I2C.show_string(0, 2, "Start->A", 1)
-#最初に実行する
-OLED12864_I2C.init(60)
-OLED12864_I2C.show_string(0, 0, "Pro. ROM3", 1)
-basic.pause(1000)
-bf = 0 #使い捨てのバッファー
-deg_180 = 0 #180゜の位置のアナログ値を入れる
-deg_0 = 0   #0゜の位置のアナログ値を入れる
-m_deg = 0   #角度の測定値を入れる
-mode = 999  #測定過程を入れる(999:準備状態 1:測定中 0:停止)
-pins.set_pull(DigitalPin.P1, PinPullMode.PULL_DOWN)
-#タクトスイッチの入力を安定化させる
-syokika() #キャリブレーション呼び出し
 
-#測定ルーチン
+m_deg = 0
+bf = 0
+deg_180 = 0
+deg_0 = 0
+mode = 0
+start_deg=0
+
+# 最初に実行する
+OLED12864_I2C.init(60)
+OLED12864_I2C.show_string(0, 0, "ROM 3-78", 1)
+basic.pause(3000)
+# 角度の測定値を入れる
+mode = 999
+# 測定過程を入れる(999:準備状態 1:測定中 0:停止)
+pins.set_pull(DigitalPin.P1, PinPullMode.PULL_DOWN)
+# タクトスイッチの入力を安定化させる
+syokika()
+# キャリブレーション呼び出し
+# 測定ルーチン
+# 測定停止
+
 def on_forever():
     global bf, m_deg, mode
-    if mode == 1: #測定中
+    if mode == 1:
+        # 測定中
         basic.show_icon(IconNames.HEART)
-        #タクトスイッチが押されるまで待つ
+        # タクトスイッチが押されるまで待つ
         while pins.digital_read_pin(DigitalPin.P1) == 0:
             pass
-        bf = pins.analog_read_pin(AnalogPin.P0) #可変抵抗の電圧を取得
-        #取得したアナログ値をキャリブレーション値で角度に変換
+        bf = pins.analog_read_pin(AnalogPin.P0)
+        # 可変抵抗の電圧を取得
+        # 取得したアナログ値をキャリブレーション値で角度に変換
         m_deg = Math.map(bf, deg_0, deg_180, 0, 180)
         music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
             music.PlaybackMode.UNTIL_DONE)
-        bf=Math.round(m_deg) #角度を四捨五入
-        string_bf=bf+" deg"
+        bf = Math.round(m_deg)
+        # 角度を四捨五入
+        string_bf = "" + str(bf) + " deg"
         OLED12864_I2C.clear()
-        OLED12864_I2C.show_string(0, 0,string_bf, 1)
-        OLED12864_I2C.show_string(0, 2,"measur->A", 1)
-        mode = 0 #測定停止
+        OLED12864_I2C.show_string(0, 0, string_bf, 1)
+        OLED12864_I2C.show_string(0, 2, "measur->A", 1)
+        mode = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,33 +1,51 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    // Aボタンで測定開始
+function get_start_position() {
     
-    mode = 1
     OLED12864_I2C.clear()
-    OLED12864_I2C.showString(0, 0, "Start Measur.", 1)
+    OLED12864_I2C.showString(0, 1, "move to St.", 1)
+    basic.showIcon(IconNames.Asleep)
+    //  タクトスイッチが押されるまで待つ
+    while (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        
+    }
+    bf = pins.analogReadPin(AnalogPin.P0)
+    // 可変抵抗の電圧を取得
+    // 取得したアナログ値をキャリブレーション値で角度に変換
+    m_deg = Math.map(bf, deg_0, deg_180, 0, 180)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    bf = Math.round(m_deg)
+    // 角度を四捨五入
+    let string_bf = bf + " deg"
+    OLED12864_I2C.clear()
+    OLED12864_I2C.showString(0, 0, "Start deg", 1)
+    OLED12864_I2C.showString(0, 1, string_bf, 1)
+    start_deg = bf
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    //  Aボタンで測定開始
+    // mode = 1
+    OLED12864_I2C.clear()
+    OLED12864_I2C.showString(0, 0, "Start Position.", 1)
+    get_start_position()
+    mode = 1
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    // A+Bボタンでキャリブレーション呼び出し
-    basic.showString("reset ROM")
+    //  A+Bボタンでキャリブレーション呼び出し
     syokika()
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    // Bボタンで測定角度再表示
-    basic.showNumber(Math.round(m_deg))
-})
 function syokika() {
-    // キャリブレーション
+    
+    //  キャリブレーション
     OLED12864_I2C.clear()
     OLED12864_I2C.showString(0, 0, "Start Cal", 1)
-    
     OLED12864_I2C.showString(0, 1, "move to 0", 1)
-    // basic.show_string("move 0")
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     while (pins.digitalReadPin(DigitalPin.P1) == 0) {
         
     }
     deg_0 = pins.analogReadPin(AnalogPin.P0)
     OLED12864_I2C.showString(0, 1, "move to 180", 1)
-    // basic.show_string("move 180")
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     while (pins.digitalReadPin(DigitalPin.P1) == 0) {
         
@@ -43,44 +61,43 @@ function syokika() {
     OLED12864_I2C.showString(0, 2, "Start->A", 1)
 }
 
-// 最初に実行する
-OLED12864_I2C.init(60)
-OLED12864_I2C.showString(0, 0, "Pro. ROM3", 1)
-basic.pause(1000)
-let bf = 0
-// 使い捨てのバッファー
-let deg_180 = 0
-// 180゜の位置のアナログ値を入れる
-let deg_0 = 0
-// 0゜の位置のアナログ値を入れる
 let m_deg = 0
-// 角度の測定値を入れる
-let mode = 999
-// 測定過程を入れる(999:準備状態 1:測定中 0:停止)
+let bf = 0
+let deg_180 = 0
+let deg_0 = 0
+let mode = 0
+let start_deg = 0
+//  最初に実行する
+OLED12864_I2C.init(60)
+OLED12864_I2C.showString(0, 0, "ROM 3-78", 1)
+basic.pause(3000)
+//  角度の測定値を入れる
+mode = 999
+//  測定過程を入れる(999:準備状態 1:測定中 0:停止)
 pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
-// タクトスイッチの入力を安定化させる
+//  タクトスイッチの入力を安定化させる
 syokika()
-// キャリブレーション呼び出し
-// 測定ルーチン
-// 測定停止
+//  キャリブレーション呼び出し
+//  測定ルーチン
+//  測定停止
 basic.forever(function on_forever() {
     let string_bf: string;
     
     if (mode == 1) {
-        // 測定中
+        //  測定中
         basic.showIcon(IconNames.Heart)
-        // タクトスイッチが押されるまで待つ
+        //  タクトスイッチが押されるまで待つ
         while (pins.digitalReadPin(DigitalPin.P1) == 0) {
             
         }
         bf = pins.analogReadPin(AnalogPin.P0)
-        // 可変抵抗の電圧を取得
-        // 取得したアナログ値をキャリブレーション値で角度に変換
+        //  可変抵抗の電圧を取得
+        //  取得したアナログ値をキャリブレーション値で角度に変換
         m_deg = Math.map(bf, deg_0, deg_180, 0, 180)
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         bf = Math.round(m_deg)
-        // 角度を四捨五入
-        string_bf = bf + " deg"
+        //  角度を四捨五入
+        string_bf = "" + ("" + bf) + " deg"
         OLED12864_I2C.clear()
         OLED12864_I2C.showString(0, 0, string_bf, 1)
         OLED12864_I2C.showString(0, 2, "measur->A", 1)
